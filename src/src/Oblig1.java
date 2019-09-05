6068dd9b9a9c7d0947fd6e367b148940e5beefca
Andreas <s333770@oslomet.no>
2019-09-05 12:22:26 +0200

    Almost done with exercise1, i need to look some more on how to calculate the effiency of the alghoritms

--- src/src/Oblig1.java
+++ src/src/Oblig1.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class Oblig1 {
 
     // Lager et globalt test-Array:
     private static int [] testArray={4,51,1,3,7,10,17,2};
+    private static int [] sortertArray={1,2,3,4,7,10,17,51};
+    private static int[] sortertGjentatt={1,1,1,2,2,2,4,4,10,10,51};
     public int testArrayLengde=testArray.length;
 
     public static int maks(int [] a){
@@ -37,10 +40,32 @@ public class Oblig1 {
         return antallOmbyttinger;
 
     }
+    public static int antallUlikeSortert(int [] a){
+        int str=sortertGjentatt.length;
+        int [] arrayMedVerdier=new int[str];
+        int teller=0;
+        for (int i=0; i<a.length; i++){
+            if(a[i]>a[i+1]){
+                throw new IllegalStateException("Arrayen er ikke sortert");
+            }
+            for(int j=0;j<i; j++){
+                if(a[i]==a[j]){
+                    break;
+                }
+                if(i==j){
+
+                }
+            }
+        }
+        System.out.println(teller);
+    return teller;
+
+    }
 
     public static void main(String[] args) {
        // Oblig1.maks(testArray); //Denne koden fungerer nå
-        Oblig1.ombyttinger(testArray); // Denne koden fungerer nå
+       // Oblig1.ombyttinger(testArray); // Denne koden fungerer nå
+        Oblig1.antallUlikeSortert(sortertGjentatt);
     }
 
 }
